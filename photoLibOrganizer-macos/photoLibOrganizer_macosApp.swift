a25f4d7ac49144ea0008f0b57b58eb8180f92a6a
Steeve Taillon <Bio80085@outlook.com>
2025-04-27 13:15:05 -0400

    Initial Commit

---
+++ photoLibOrganizer-macos/photoLibOrganizer_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoLibOrganizer_macosApp.swift
+//  photoLibOrganizer-macos
+//
+//  Created by Steeve Taillon on 2025-04-27.
+//
+
+import SwiftUI
+
+@main
+struct photoLibOrganizer_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ photoLibOrganizer-macosTests/photoLibOrganizer_macosTests.swift
@@ -0,0 +1,17 @@
+//
+//  photoLibOrganizer_macosTests.swift
+//  photoLibOrganizer-macosTests
+//
+//  Created by Steeve Taillon on 2025-04-27.
+//
+
+import Testing
+@testable import photoLibOrganizer_macos
+
+struct photoLibOrganizer_macosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
